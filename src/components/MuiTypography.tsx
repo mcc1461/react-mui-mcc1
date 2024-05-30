0bfa2d26b9b5e28bf54b5612df7012e5e020e707
mcc1461 <mcoskuncelebi@gmail.com>
2024-05-30 18:38:47 +0300

    feat: typography recaped

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,21 @@
+import { Typography } from '@mui/material'
+
+export const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant="h1">h1. Heading</Typography>
+        <Typography variant="h2">h2. Heading</Typography>
+        <Typography variant="h3">h3. Heading</Typography>
+        <Typography variant="h4" component="h1" gutterBottom>h4. Heading</Typography>
+        <Typography variant="h5">h5. Heading</Typography>
+        <Typography variant="h6">h6. Heading</Typography>
+        <Typography variant="subtitle1">h3. Heading</Typography>
+        <Typography variant="subtitle2">h3. Heading</Typography>
+        <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque animi blanditiis, voluptatibus, nulla consequuntur ex reprehenderit quidem ea ullam, fugit iusto laudantium voluptatum? Perspiciatis iusto distinctio nemo modi explicabo iste cum nesciunt nam optio doloribus?</Typography>
+        <Typography variant="body2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et voluptate dolorem placeat quae obcaecati quos quod animi aliquam! Nisi quo fuga laborum, eveniet cum blanditiis quasi! Doloremque, facere amet perspiciatis voluptatibus aliquid adipisci molestias quo quis magni laboriosam hic similique nihil laborum ab eaque voluptatem?</Typography>
+        
+    </div>
+  )
+}
+
+export default MuiTypography
