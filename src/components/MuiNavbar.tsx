5c23d264b9e96e1c793ff69f2e22583fe6de2bbb
mcc1461 <mcoskuncelebi@gmail.com>
2024-06-04 00:10:10 +0300

    feat: Navbar and Menu are covered

--- src/components/MuiNavbar.tsx
+++ src/components/MuiNavbar.tsx
@@ -5,19 +5,32 @@ import {
   Typography,
   Stack,
   Button,
-  ButtonGroup,
-  Box,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
+import { useState } from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";  
+
 
 const MuiNavbar = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    // alert("clicked");
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
+
   return (
     <AppBar position={"static"}>
       <Toolbar>
         <IconButton size="large" edge="start" color="inherit" aria-label="logo">
           <CatchingPokemonIcon />
         </IconButton>
-        <Typography variant="h6" component={"div"} sx={{flexGrow:1}}>
+        <Typography variant="h6" component={"div"} sx={{ flexGrow: 1 }}>
           PokemonApp
         </Typography>
         <Stack
@@ -30,18 +43,50 @@ const MuiNavbar = () => {
           }}
         >
           <Button color="inherit" variant="outlined">
-            Home
+            Features
           </Button>
           <Button color="warning" variant="contained">
-            Services
+            Pricing
           </Button>
           <Button color="inherit" variant="outlined">
-            Contact
+            About
+          </Button>
+          <Button
+            id="resources-button"
+            color="inherit"
+            variant="outlined"
+            onClick={handleClick}
+            aria-controls={open ? "resources-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            endIcon={<KeyboardArrowDownIcon />}
+          >
+            Resources
           </Button>
           <Button color="inherit" variant="outlined">
             Login
           </Button>
         </Stack>
+        <Menu
+          id="resouces-menu"
+          anchorEl={anchorEl}
+          open={open}
+          MenuListProps={{
+            "aria-labelledby": "resources-button",
+          }}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+            transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+            }}
+        >
+          <MenuItem onClick={handleClose}>Blog</MenuItem>
+          <MenuItem onClick={handleClose}>Podcast</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
